8551cfbec93054461f7fcc47573fdc7e95e00278
Yeison <yencardonaal@unal.edu.co>
2024-08-04 14:18:13 -0500

    Added swagger

--- orchdb/settings.py
+++ orchdb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    'drf_yasg',
     'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
